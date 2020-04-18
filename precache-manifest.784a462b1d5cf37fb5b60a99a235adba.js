fc9b98ff23423e1e32040a2bd37ea58ee3770f9b
Kashishdwn <kashishdwn@gmail.com>
2020-04-19 01:16:11 +0530

    Updates

---
+++ precache-manifest.784a462b1d5cf37fb5b60a99a235adba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f3787b8eb7cfdd0d9935a66b6e087c",
+    "url": "/Dice-Game/index.html"
+  },
+  {
+    "revision": "840d28faec86dd937f8e",
+    "url": "/Dice-Game/static/css/main.86233756.chunk.css"
+  },
+  {
+    "revision": "a5819b33f56a1cbabe56",
+    "url": "/Dice-Game/static/js/2.ec4b8a3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dice-Game/static/js/2.ec4b8a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840d28faec86dd937f8e",
+    "url": "/Dice-Game/static/js/main.4ab9aee0.chunk.js"
+  },
+  {
+    "revision": "aa5a87ea42cbf65016b1",
+    "url": "/Dice-Game/static/js/runtime-main.a461b3d7.js"
+  }
+]);
